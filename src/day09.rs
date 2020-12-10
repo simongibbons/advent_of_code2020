bd1f2bf260e5b5a39585031f897e46b4f7b9facd
Simon Gibbons <simongibbons@gmail.com>
2020-12-10 08:31:22 +0000

    day9 part2

--- src/day09.rs
+++ src/day09.rs
@@ -9,6 +9,28 @@ fn two_sum(numbers: &[i64], target: i64) -> Option<(i64, i64)> {
     None
 }
 
+fn find_range_that_sums(numbers: &[i64], target: i64) -> Option<&[i64]> {
+    let mut lo = 0;
+    let mut hi = 0;
+    let mut sum = 0;
+
+    loop {
+        if sum == target {
+            return Some(&numbers[lo..hi]);
+        } else if sum < target {
+            hi += 1;
+            if hi > numbers.len() {
+                break;
+            }
+            sum += numbers[hi - 1];
+        } else {
+            lo += 1;
+            sum -= numbers[lo - 1];
+        }
+    }
+    None
+}
+
 #[aoc_generator(day9)]
 pub fn parse_numbers(input: &str) -> Vec<i64> {
     input.split('\n')
@@ -25,4 +47,11 @@ pub fn part1(numbers: &[i64]) -> i64 {
         }
     }
     panic!("No solution")
+}
+
+#[aoc(day9, part2)]
+pub fn part2(numbers: &[i64]) -> i64 {
+    let target = part1(numbers);
+    let result = find_range_that_sums(numbers, target).unwrap();
+    result.iter().min().unwrap() + result.iter().max().unwrap()
 }
